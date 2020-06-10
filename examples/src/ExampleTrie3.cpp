e533fe2a47d74359b88fc11b148f76aae23ec7d2
George Fotopoulos <xorz57@protonmail.com>
2020-06-10 23:26:13 +0300

    Refactor

--- examples/src/ExampleTrie3.cpp
+++ examples/src/ExampleTrie3.cpp
@@ -3,24 +3,24 @@
 
 using namespace std;
 
-int main(){
-    forest::Trie<wchar_t> Trie;
+int main() {
+  forest::Trie<wchar_t> Trie;
 
-    Trie.insert(L"కంప్యూటర్");
-    Trie.insert(L"கணக்கிடும் பொறி");
-    Trie.insert(L"संगणक");
-    Trie.insert(L"Computer");
-    Trie.insert(L"υπολογιστή");
-    Trie.insert(L"компьютер");
+  Trie.insert(L"కంప్యూటర్");
+  Trie.insert(L"கணக்கிடும் பொறி");
+  Trie.insert(L"संगणक");
+  Trie.insert(L"Computer");
+  Trie.insert(L"υπολογιστή");
+  Trie.insert(L"компьютер");
 
-    cout << Trie.size() << endl;
-    cout << Trie.search(L"கணக்கிடும் பொற")  << endl;
-    cout << Trie.search(L"కంప్యూటర్")  << endl;
-    cout << Trie.remove(L"комп")  << endl;
+  cout << Trie.size() << endl;
+  cout << Trie.search(L"கணக்கிடும் பொற") << endl;
+  cout << Trie.search(L"కంప్యూటర్") << endl;
+  cout << Trie.remove(L"комп") << endl;
 
-    cout << Trie.remove(L"υπολογιστή")  << endl;
-    
-    Trie.insert(L"Computer Science");
-    cout << Trie.size() << endl;
-    cout << Trie.search(L"संगणक")  << endl;
+  cout << Trie.remove(L"υπολογιστή") << endl;
+
+  Trie.insert(L"Computer Science");
+  cout << Trie.size() << endl;
+  cout << Trie.search(L"संगणक") << endl;
 }
