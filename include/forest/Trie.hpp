357599a4a15001356a8a3be4bc894b4baa26bc79
George Fotopoulos <xorz57@protonmail.com>
2020-06-10 21:47:55 +0300

    Refactor Trie.hpp

--- include/forest/Trie.hpp
+++ include/forest/Trie.hpp
@@ -8,33 +8,33 @@
 namespace forest {
 template <typename T> class Trie {
 private:
-  struct TrieNode {
-    std::unordered_map<T, std::shared_ptr<TrieNode>> children;
+  struct Node {
+    std::unordered_map<T, std::shared_ptr<Node>> children;
     bool end = false;
   };
-  std::shared_ptr<TrieNode> root = std::make_shared<TrieNode>();
-  size_t numWords = 0;
+  std::shared_ptr<Node> root = std::make_shared<Node>();
+  std::size_t mSize = 0;
 
 public:
   Trie() = default;
 
-  auto size() { return this->numWords; }
+  auto size() { return this->mSize; }
 
   void insert(const std::basic_string<T> &key) {
-    std::shared_ptr<TrieNode> current = root;
+    std::shared_ptr<Node> current = root;
     for (const T &c : key) {
       if (current->children.find(c) == current->children.end())
-        current->children[c] = std::make_shared<TrieNode>();
+        current->children[c] = std::make_shared<Node>();
       current = current->children[c];
     }
     if (!current->end && current != root) {
       current->end = true;
-      ++this->numWords;
+      ++this->mSize;
     }
   }
 
   bool search(const std::basic_string<T> &key) {
-    std::shared_ptr<TrieNode> current = root;
+    std::shared_ptr<Node> current = root;
     for (const T &c : key) {
       if (current->children.empty())
         return false;
@@ -46,40 +46,32 @@ public:
   }
 
   bool remove(const std::basic_string<T> &key) {
-
     if (root->children.empty())
       return false;
-
-    std::shared_ptr<TrieNode> current = root;
-    std::stack<std::shared_ptr<TrieNode>> stk;
-
-    stk.push(current);
-
+    std::shared_ptr<Node> current = root;
+    std::stack<std::shared_ptr<Node>> stack;
+    stack.push(current);
     for (const T &c : key) {
       if (current->children.empty())
         return false;
       current = current->children[c];
       if (!current)
         return false;
-      stk.push(current);
+      stack.push(current);
     }
-
     // If No Prefix is matched or The prefix is not a Legit word.
-    if (stk.size() <= 1 || !stk.top()->end)
+    if (stack.size() <= 1 || !stack.top()->end)
       return false;
-
-    stk.top()->end = false;
-    auto r_itr = key.rbegin();
-
+    stack.top()->end = false;
+    auto r_it = key.rbegin();
     // Remove a Node Iteratively when it is not a word and it has no children.
-    while (!stk.top()->end && stk.top()->children.empty() &&
-           r_itr != key.rend()) {
-      stk.pop();
-      stk.top()->children.erase(*r_itr);
-      ++r_itr;
+    while (!stack.top()->end && stack.top()->children.empty() &&
+           r_it != key.rend()) {
+      stack.pop();
+      stack.top()->children.erase(*r_it);
+      ++r_it;
     }
-
-    --this->numWords;
+    --this->mSize;
     return true;
   }
 };
